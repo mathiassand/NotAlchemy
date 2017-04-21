88a078970f2da8761d261bfd6c3a643c98f9dea8
msk4 <mskr16@student.aau.dk>
2017-04-21 13:40:49 +0200

    ButtonGONE

--- NotAlchemy/app/src/main/java/com/example/larsnielsen/notalchemy/MainActivity.java
+++ NotAlchemy/app/src/main/java/com/example/larsnielsen/notalchemy/MainActivity.java
@@ -3,29 +3,31 @@ package com.example.larsnielsen.notalchemy;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+
+
+
     public void buttonPress(View v) {
-        switch (v.getId()) {
+        switch (v.getId()){
             case R.id.button:
-                // do something
+                Button buttonOne = (Button) findViewById(R.id.button);
+                buttonOne.setVisibility(View.GONE);
                 break;
-            case R.id.button2:
-                // do something else
-                break;
-            case R.id.button3:
-                // i'm lazy, do nothing
-                break;
+
         }
-    }
-}
+    } }
+
 /*
 playButton = (Button) findViewById(R.id.play);
         playButton.setVisibility(1);
