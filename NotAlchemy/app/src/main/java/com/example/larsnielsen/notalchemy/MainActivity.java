c70367ccea595ba160bb449ca48cea40e0c18f80
Bakinak <akobbe16@student.aau.dk>
2017-04-21 12:09:00 +0200

    hej

--- NotAlchemy/app/src/main/java/com/example/larsnielsen/notalchemy/MainActivity.java
+++ NotAlchemy/app/src/main/java/com/example/larsnielsen/notalchemy/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.larsnielsen.notalchemy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonPress(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                // do something
+                break;
+            case R.id.button2:
+                // do something else
+                break;
+            case R.id.button3:
+                // i'm lazy, do nothing
+                break;
+        }
     }
 }
